2564f0036f6f42b92c531a04c0408b9262d81f9d
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-03-24 01:11:15 +0430

    build html file and create constructor class

---
+++ script.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+// Book Class: Represents a Book
+// eslint-disable-next-line max-classes-per-file
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
